fe17a67d668e4f6fa43e25ff0a0fd1d1651db4a7
Lawre <camperinc@gmail.com>
2018-03-14 20:26:07 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+     this.result = initialValue;
+        this.operations = [];
+  }
+    getResult() {
+        const action = this.operations.reduce(function(result, item) {
+            return result + item.action + item.value;
+        }, this.result.toString());
+
+        return eval(action);
   }
 
-  add(number) {
-    // your implementation
+    addOperation(action, value) {
+        this.operations.push({
+            action: action,
+            value: value
+        });
+  }
+
+  add(number){
+       this.addOperation('+', number);
+        return this;
   }
   
   subtract(number) {
-    // your implementation
-  }
+     this.addOperation('-', number);
+        return this;
+    }
 
   multiply(number) {
-    // your implementation
-  }
+      this.addOperation('*', number);
+        return this;
+    }
 
   devide(number) {
-    // your implementation
-  }
+        this.addOperation('/', number);
+        return this;
+    }
 
   pow(number) {
-    // your implementation
-  }
+      this.addOperation('**', number);
+        return this;
+    }
+
+    toString() {
+        return this.getResult();
+    }
 }
 
 module.exports = SmartCalculator;
